833cc6d796efab191684510852b12bf65e71dfcf
Antonio Greco <38771514+fosseperme@users.noreply.github.com>
2018-05-09 03:05:37 +0200

    added SnapdragonMusic

--- msm8953.mk
+++ msm8953.mk
@@ -320,6 +320,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Snap
+PRODUCT_PACKAGES += \
+    SnapdragonMusic
+     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
 
